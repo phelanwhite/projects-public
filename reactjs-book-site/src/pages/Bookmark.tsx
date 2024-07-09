599592db3a9b62127c26d575f119ed365b72bc5d
phelan-white <phelanwhite.business@gmail.com>
2024-07-09 15:04:21 +0700

    first commit

---
+++ reactjs-book-site/src/pages/Bookmark.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Bookmark = () => {
+  return (
+    <div className="my-8 flex flex-col md:flex-row md:items-start gap-8 wrapper">
+      Bookmark
+    </div>
+  );
+};
+
+export default Bookmark;
