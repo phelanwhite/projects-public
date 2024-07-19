2e7b9b77b93e05183b915d7ef43845bf5a42cbd0
phelan-white <phelanwhite.business@gmail.com>
2024-07-20 06:54:27 +0700

    mern-movie-site

--- mern-movie-site/backend/index.js
+++ mern-movie-site/backend/index.js
@@ -26,7 +26,9 @@ app.use(bodyParser.json())
 app.use(router)
 
 // deploy
-if (process.env.NODE_ENV === "production") {
+if (
+  process.env.NODE_ENV === "production"
+) {
   const __dirname= path.resolve()
   app.use(express.static(path.join(__dirname, `/frontend/dist`)));
   app.get("*", (req, res) => {
