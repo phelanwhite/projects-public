11061bba0abe5330eb79bd88d406be2c7743af8f
phelan-white <phelanwhite.business@gmail.com>
2024-07-19 16:19:13 +0700

    test

--- test/backend/index.js
+++ test/backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from 'cors'
 import cookieParser from "cookie-parser";
+import dotenv from 'dotenv'
+dotenv.config()
 import path from 'path'
 
 const app = express();
